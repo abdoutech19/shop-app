1a5c0092d69f5d1c44c96e62d247b95619e4f865
Abdenour Kebouchi <abdou19.tech@gmail.com>
2021-07-12 18:35:07 +0100

    OneSignal SDK setup

--- App.js
+++ App.js
@@ -8,6 +8,10 @@ import {Provider as AuthProvider} from './src/context/auth/AuthContext';
 import {Provider as ProductProvider} from './src/context/product/ProductContext';
 import {Provider as CartProvider} from './src/context/cart/CartContext';
 import {Provider as OrdersProvider} from './src/context/orders/OrdersContext';
+import OneSignal from 'react-native-onesignal';
+
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId('95ee0575-c468-4644-a3c6-c2e1b0379b0d');
 
 enableScreens();
 
